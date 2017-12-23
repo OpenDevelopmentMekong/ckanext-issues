1e473b7a091d0eb6e01c07e70c9dd355055bec04
Alex Corbi <a.corbi@gmail.com>
2017-12-23 11:21:44 +0100

    Fixes #29

--- ckanext/issues/auth.py
+++ ckanext/issues/auth.py
@@ -52,7 +52,7 @@ def _issue_auth_status_change(context,data_dict):
           'msg': p.toolkit._('Dataset owner is not allowed to change status of issue {0}'.format(issue_id))
         }
 
-    result = issue_auth_organization(context, data_dict, 'organization_update'):
+    result = issue_auth_organization(context, data_dict, 'organization_update')
     if not result.success:
         return {
           'success': False,
